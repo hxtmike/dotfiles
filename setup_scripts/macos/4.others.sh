f17ddd788b0b49d8f4f523ef77715e90fd1ad9ab
Sean Han <19608780+hxtmike@users.noreply.github.com>
2025-12-29 11:50:53 +0000

    Update 4.others.sh

--- setup_scripts/macos/4.others.sh
+++ setup_scripts/macos/4.others.sh
@@ -6,3 +6,5 @@ fi
 # macos setting
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -boolean false # press and hold disable
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false   
+
+defaults write -g KeyRepeat -int 1
